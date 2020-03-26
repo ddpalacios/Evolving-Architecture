06263cfed5eb38382d4b91119b6cc18676a204d8
Daniel <palaciosdanieldario@gmail.com>
2020-03-26 10:00:03 -0500

    Approaching dog_vs_cat dataset with codeepneat

--- __main__.py
+++ __main__.py
@@ -1,9 +1,9 @@
 from Setup_ import Setup
 import os
-import numpy as np
-from create_population import Create_Population
+# import numpy as np
+# from create_population import Create_Population
 import imp
-from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger
+# from keras.callbacks import EarlyStopping, ModelCheckpoint, CSVLogger
 
 def create_dir(dir):
     if not os.path.exists(os.path.dirname(dir)):
@@ -28,7 +28,7 @@ class load_file:
 
 
 if __name__ == "__main__":
-    kerascodeepneat = imp.load_source("kerascodeepneat", "/home/daniel/github/Keras-CoDeepNEAT/base/kerascodeepneat.py")
+    # kerascodeepneat = imp.load_source("kerascodeepneat", "/home/daniel/github/Evolving-Architecture/base/kerascodeepneat.py")
 
     generations = 2
     training_epochs = 2
@@ -42,49 +42,46 @@ if __name__ == "__main__":
     batch_size = 128
 
 
-
-    df = load_file('halloween_classes.npz')
     create_dir("models/")
     create_dir("images/")
 
     setup = Setup()
+    data_dir = 'dogs_vs_cats/train/'
+    setup.dataset(data_dir)
+#     compiler = setup.compiler_(lr=.001)
 
-    x_train, y_train, x_test, y_test, datagen = setup.dataset(df.get_data())
-    compiler = setup.compiler_(lr=.001)
 
-
-    my_dataset = kerascodeepneat.Datasets(training=[x_train, y_train], test=[x_test, y_test])
-   
-    es = EarlyStopping(monitor='val_acc', mode='auto', verbose=1, patience=15)
-    csv_logger = CSVLogger('training.csv')
+#     my_dataset = kerascodeepneat.Datasets(training=[x_train, y_train], test=[x_test, y_test])
+#     print(my_dataset)
+#     es = EarlyStopping(monitor='val_acc', mode='auto', verbose=1, patience=15)
+#     csv_logger = CSVLogger('training.csv')
     
-    custom_fit_args = {"generator": datagen.flow(x_train, y_train, 
-                        batch_size=batch_size),
-                        "steps_per_epoch": x_train.shape[0] // batch_size,
-                        "epochs": training_epochs,
-                        "verbose": 1,
-                        "validation_data": (x_test,y_test),
-                        "callbacks": [es, csv_logger]
-}       
+#     custom_fit_args = {"generator": datagen.flow(x_train, y_train, 
+#                         batch_size=batch_size),
+#                         "steps_per_epoch": x_train.shape[0] // batch_size,
+#                         "epochs": training_epochs,
+#                         "verbose": 1,
+#                         "validation_data": (x_test,y_test),
+#                         "callbacks": [es, csv_logger]
+# }       
 
-    improved_dataset = kerascodeepneat.Datasets(training=[x_train, y_train], test=[x_test, y_test])
-    improved_dataset.custom_fit_args = custom_fit_args
-    my_dataset.custom_fit_args = None
+#     improved_dataset = kerascodeepneat.Datasets(training=[x_train, y_train], test=[x_test, y_test])
+#     improved_dataset.custom_fit_args = custom_fit_args
+#     my_dataset.custom_fit_args = None
 
     
-    population = kerascodeepneat.Population(my_dataset, 
-                                            input_shape=x_train.shape[1:], 
-                                            population_size=population_size, 
-                                            compiler=compiler)
+#     population = kerascodeepneat.Population(my_dataset, 
+#                                             input_shape=x_train.shape[1:], 
+#                                             population_size=population_size, 
+#                                             compiler=compiler)
 
-    create = Create_Population(population, num_of_classes=2)
+#     create = Create_Population(population, num_of_classes=2)
 
-    create.modules(module_population_size, n_module_species)
-    create.blueprints(blueprint_population_size, n_blueprint_species)
+#     create.modules(module_population_size, n_module_species)
+#     create.blueprints(blueprint_population_size, n_blueprint_species)
     
-    iteration = population.hello()
 
-    # iteration = population.iterate_generations()
+#     iteration = population.iterate_generations()
                                                
 
 
